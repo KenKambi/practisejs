dd49eebbd2c57543ac04a8e25e5de346575a248c
Ken Kambi <kenkambi43@gmail.com>
2024-11-26 19:41:04 +0300

    Done script.js on my own

--- counter/script.js
+++ counter/script.js
@@ -1,1 +1,25 @@
 //alert('Helli');
+
+//bring the html to javascript
+const runNumber = document.querySelector('.js-number');
+const decrease = document.getElementById('dec');
+const reset = document.getElementById('reset');
+const increase = document.getElementById('inc');
+
+//functions for each button on click event
+decrease.addEventListener('click', () => {
+    const ans = (runNumber.textContent-1);
+    console.log(ans);
+    runNumber.textContent = ans;
+});
+
+reset.addEventListener('click', () => {
+    runNumber.textContent = 0;
+});
+
+increase.addEventListener('click', () => {
+    const currentNo = parseInt(runNumber.textContent);
+    const newNo = (currentNo + 1);
+    runNumber.textContent = newNo;
+    console.log(newNo);
+});
