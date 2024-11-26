58537c0190e2cba803a5d324a4bae8cdbbd43289
Ken Kambi <kenkambi43@gmail.com>
2024-11-26 16:31:50 +0300

    random hex background-color

--- practise1/script.js
+++ practise1/script.js
@@ -1,7 +1,29 @@
 //alert('Hello');
-const hex = ['red', 'green', 'blue', 'lightpink'];
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+    'A', 'B', 'C', 'D', 'E', 'F'];
+
+const text = document.querySelector('.js-hex-code');
 
 document.getElementById('click').addEventListener('click', () => {
-    console.log(hex[3]);
-    document.body.style.backgroundColor = hex[3];
+    //generate other 6 values for the hexcode#
+    let hexy = '#';
+    for(let i=0; i<6; i++){
+        hexy += hex[getRandomNumber()];
+    }
+
+    //change the background color on html
+    document.body.style.backgroundColor = hexy;
+    text.textContent = hexy;
+
+    console.log (hex[getRandomNumber]);
+    
 });
+
+function getRandomNumber(){
+    return Math.floor(Math.random()*hex.length);
+    
+}
+
+//console.log( Math.floor(Math.random()*hex.length));
+
+
