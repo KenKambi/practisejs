a1921b1403d028d66c2d9f16b9dc8eed08ae8558
Ken Kambi <kenkambi43@gmail.com>
2025-02-04 22:31:40 +0300

    VIMPractise

--- reviews/reviews.js
+++ reviews/reviews.js
@@ -1,63 +1,54 @@
-
-const peoples = [{
+const peoples = [
+  {
     name: "Peter Jones",
     rank: "Intern",
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sagittis iaculis iaculis. Nullam feugiat fringilla varius. Ut vitae arcu quis dui malesuada facilisis."
-
-},
-{
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sagittis iaculis iaculis. Nullam feugiat fringilla varius. Ut vitae arcu quis dui malesuada facilisis.",
+  },
+  {
     name: "Jane Ros",
     rank: "Intern",
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sagittis iaculis iaculis. Nullam feugiat fringilla varius. Ut vitae arcu quis dui malesuada facilisis."
-
-},
-{
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sagittis iaculis iaculis. Nullam feugiat fringilla varius. Ut vitae arcu quis dui malesuada facilisis.",
+  },
+  {
     name: "Doe John",
     rank: "Intern",
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sagittis iaculis iaculis. Nullam feugiat fringilla varius. Ut vitae arcu quis dui malesuada facilisis."
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sagittis iaculis iaculis. Nullam feugiat fringilla varius. Ut vitae arcu quis dui malesuada facilisis.",
+  },
+];
 
-}];
-
-const nameOfPerson = document.querySelector('.name');
-
+const nameOfPerson = document.querySelector(".name");
 
 let currentItem = 0;
 
-window.addEventListener('DOMContentLoaded', () => {
-    showPerson();
+window.addEventListener("DOMContentLoaded", () => {
+  showPerson();
 });
 
-function showPerson(){
-    const item = peoples[currentItem];
-    nameOfPerson.textContent = item.name;
+function showPerson() {
+  const item = peoples[currentItem];
+  nameOfPerson.textContent = item.name;
 }
 
-document.getElementById('suprise').addEventListener('click', () => {
-    console.log('Random');
-    currentItem = (Math.floor(Math.random() * peoples.length));
-    showPerson();
+document.getElementById("suprise").addEventListener("click", () => {
+  console.log("Random");
+  currentItem = Math.floor(Math.random() * peoples.length);
+  showPerson();
 });
 
-document.getElementById('back').addEventListener('click', () => {
-    console.log('Back');
-    let back = peoples[currentItem --];
-    if (currentItem < 0 ){
-        currentItem = peoples.length -1;
-    };
-    showPerson();
-
-
-
+document.getElementById("back").addEventListener("click", () => {
+  console.log("Back");
+  let back = peoples[currentItem--];
+  if (currentItem < 0) {
+    currentItem = peoples.length - 1;
+  }
+  showPerson();
 });
 
-document.getElementById('next').addEventListener('click', () => {
-    console.log('Next');
-    let next = peoples[currentItem ++];
-    if (currentItem > peoples.length -1 ){
-        currentItem = 0;
-    };
-    showPerson();
-
-})
-
-
+document.getElementById("next").addEventListener("click", () => {
+  console.log("Next");
+  let next = peoples[currentItem++];
+  if (currentItem > peoples.length - 1) {
+    currentItem = 0;
+  }
+  showPerson();
+});
