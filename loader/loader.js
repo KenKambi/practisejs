7e9c6264edcff67360f5155a3fb6778c3d7ee36b
Ken Kambi <kenkambi43@gmail.com>
2025-02-13 11:40:41 +0300

    preloaderGIF

--- loader/loader.js
+++ loader/loader.js
@@ -1,12 +1,15 @@
 //alert("Hello");
 
-const videoControl = document.querySelector(".video");
-const btnControl = document.querySelector(".btn");
-const preloader = document.querySelector(".preloader");
+let videoControl = document.querySelector(".video");
+let btnControl = document.querySelector(".btn");
+let preloader = document.querySelector(".preloader");
+let txt = document.querySelector('.text');
 
 btnControl.addEventListener("click", () => {
   videoControl.pause();
+  //console.log(preloader.classList.toggle('hide-preloader'));
   console.log("Video Paused");
+  txt.classList.toggle('text-change');
 });
 
 window.addEventListener("load", () => {
